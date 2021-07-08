de7acbe17d93beb194e39d05fe9f2c2511a7f589
ygorp <ygor-nogueira@hotmail.com>
2021-07-08 17:23:01 -0300

    definindo as reações e impedindo que va embora pelas bordas

--- script.js
+++ script.js
@@ -21,27 +21,40 @@ function criarCobrinha(){
   }
 }
 
+document.addEventListener('keydown', update);
+
+function update (event){
+  if(event.keyCode == 37 && direction != "right") direction = "left";
+  if(event.keyCode == 38 && direction != "down") direction = "up";      //direcionando através dos botões do teclado, só vai movimentar se for para posição correta.
+  if(event.keyCode == 39 && direction != "left") direction = "right";   //a função update vai chamar a event, toda vez que teclarmos uma direção.
+  if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarjogo(){
+  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0; //Quando a cobrinha atingir o tamanho de 15 que é o tamanho do canva, ela vai receber o valor de 0 que é da esquerda para direita, fazendo ela aparcer do outro lado.
+  if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
   criarBG();
   criarCobrinha();
 
-  let snakex = snake[0].x; //posição da cobrinha quando executar os movimentos.
-  let snakey = snake[0].y;
+  let snakeX = snake[0].x; //posição da cobrinha quando executar os movimentos.
+  let snakeY = snake[0].y;
 
-  if(direction == "right") snakex += box; // para direita aumenta para ver que esta indo para direita
-  if(direction == "left") snakex -= box; // para esquerda diminui para ver que esta indo para esquerda (plano cartesiano)
-  if(direction == "up") snake -= box;
-  if(direction == "down") snake += box;
+  if(direction == "right") snakeX += box; // para direita aumenta para ver que esta indo para direita
+  if(direction == "left") snakeX -= box; // para esquerda diminui para ver que esta indo para esquerda (plano cartesiano)
+  if(direction == "up") snakeY -= box;
+  if(direction == "down") snakeY += box;
 
   snake.pop(); //retira o ultimo elemento do nosso array
 
   let newhead = {
-    x:snakex,
-    y:snakey
+    x:snakeX,
+    y:snakeY
   }
 
   snake.unshift(newhead);
 }
 
 let jogo = setInterval(iniciarjogo, 100) // intervalo de 100 mili segundos para inciar o jogo, e a cada 100 mili segundos continuar o jogo sem travar
-
